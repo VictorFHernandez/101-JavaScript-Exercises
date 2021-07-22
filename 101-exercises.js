6a31ec87f721a6240eaaea599ae6204c583784e5
VictorFHernandez <victorfhernandez3@gmail.com>
2021-07-21 21:11:26 -0500

    finished exercise 60

--- 101-exercises.js
+++ 101-exercises.js
@@ -968,6 +968,14 @@ addToDone("Exercise 59 is correct.");
 // Exercise 60
 // Write a function definition named sumAll that takes in sequence of numbers and returns all the numbers added together.
 
+function sumAll(sequence){
+    var sum = 0;
+    for(var i = 0; i < sequence.length; i++){
+        sum += Number(sequence[i])
+    }
+    return sum
+}
+
 assert(sumAll([1, 2, 3, 4]), 10, "Exercise 60");
 assert(sumAll([3, 3, 3]), 9, "Exercise 60");
 assert(sumAll([0, 5, 6]), 11, "Exercise 60");
