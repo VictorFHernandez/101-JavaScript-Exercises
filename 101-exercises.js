d49facfcf3d2957946970955d08b11ee41f41c2a
VictorFHernandez <victorfhernandez3@gmail.com>
2021-07-22 22:59:16 -0500

    finished exercise 61

--- 101-exercises.js
+++ 101-exercises.js
@@ -984,6 +984,14 @@ addToDone("Exercise 60 is correct.");
 //  Exercise 61
 //  Write a function definition named mean that takes in sequence of numbers and returns the average value
 
+function mean(sequence){
+    var sum = 0;
+    for(var i = 0; i < sequence.length; i++){
+        sum += Number(sequence[i])
+    }
+    return sum/sequence.length;
+}
+
 assert(mean([1, 2, 3, 4]), 2.5, "Exercise 61");
 assert(mean([3, 3, 3]), 3, "Exercise 61");
 assert(mean([1, 5, 6]), 4, "Exercise 61");
